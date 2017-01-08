4c34688a6f922b305ca4492f7b7447c0beacc5ea
drkanukie <drkanukie@gmail.com>
2017-01-08 20:31:14 +0000

    local store cracked

--- js/functions.js
+++ js/functions.js
@@ -21,19 +21,57 @@ function getTotal() {
   }
 }
 
-function saveForm(id) {
+function saveForm(form_id) {
   if (typeof(Storage) !== "undefined") {
-    return localStorage.getItem(basket_key);
+    var form_html;
+    form_html = document.getElementById(form_id).innerHTML;
+    console.log("SAVED " + form_html);
+    return localStorage.setItem(form_key, form_html);
   } else {
     return 0;
   }
 }
 
 
-function restoreForm(id) {
+function restoreForm(form_id) {
   if (typeof(Storage) !== "undefined") {
-    return localStorage.getItem(form_key);
+    var form_html;
+    form_html = localStorage.getItem(form_key);
+    console.log("RESTORED " + form_html);
+    if (form_html) {
+      document.getElementById(form_id).innerHTML = form_html;
+    }
   } else {
     return 0;
   }
+}
+
+function saveRadio() {
+  localStorage.clear();
+
+  $('input[type="radio"]').each(function() {
+
+
+    if ($(this).is(':checked')) {
+
+      var id = $(this).attr('id');
+      var value = $(this).prop('checked', true);
+      console.log("SAVING " + id + " " + value);
+      localStorage.setItem(id, value);
+    }
+
+
+  });
+}
+
+function restoreRadio() {
+  $('input[type="radio"]').each(function() {
+    var id = $(this).attr('id');
+    var value = localStorage.getItem(id);
+
+    if (value) {
+      console.log("RESTORING " + id + " " + value);
+      $(this).prop('checked', value);
+    }
+  });
 }
